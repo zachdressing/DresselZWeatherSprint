cc2b1610830968245cea3a206a579b9e39e46577
zachdressing <zed_dressel@hotmail.com>
2023-12-04 16:05:45 -0800

    Added API call for weather

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,25 @@
+//import Key
+import {apiKey} from "./environment.js"
+
 //grab date
 const date = new Date();
 
-//grab formatted dates
-let longDay = date.toLocaleDateString('default', {weekday: 'long'});
-let shortDay = date.toLocaleDateString('default', {day: 'numeric'})
-let month = date.toLocaleDateString('default', {month: 'long'});
+//format dates
+let longDay = date.toLocaleDateString('default', { weekday: 'long' });
+let shortDay = date.toLocaleDateString('default', { day: 'numeric' })
+let month = date.toLocaleDateString('default', { month: 'long' });
 
 //Log to confirm working
 console.log(month);
 console.log(longDay);
 console.log(shortDay);
+
+//fetch the API for weather
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=37.9577&lon=121.2908&appid=${apiKey}`)
+        .then((response) => {
+            return response.json
+        })
+        .then((data) => {
+            console.log(data);
+        })
+
