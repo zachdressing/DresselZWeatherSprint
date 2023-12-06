e30db44683c1a23c57b0eaf6e47db0ab7264e48b
zachdressing <zed_dressel@hotmail.com>
2023-12-06 11:45:48 -0800

    Added Search Functionality

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,7 @@ let dateNav = document.getElementById('dateNav');
 let centerDay = document.getElementById('centerDay');
 let centerLocation = document.getElementById('centerLocation')
 let searchVal = document.getElementById('search').value;
+let searchBar = document.getElementById('search');
 let centerTemp = document.getElementById('centerTemp');
 let minMax = document.getElementById('minMax');
 let day2Day = document.getElementById('day2Day')
@@ -25,19 +26,25 @@ let day6High = document.getElementById('day6High')
 let day6Low = document.getElementById('day6Low')
 
 //grab date
-const date = new Date();
+let date = new Date();
+const date2 = new Date(date.setDate(date.getDate() + 1))
+const date3 = new Date(date.setDate(date.getDate() + 1))
+const date4 = new Date(date.setDate(date.getDate() + 1))
+const date5 = new Date(date.setDate(date.getDate() + 1))
+const date6 = new Date(date.setDate(date.getDate() + 1))
+let currentDate = new Date();
 
 //format dates
-let longDay = date.toLocaleDateString('default', { weekday: 'long' });
-let shortDay = date.toLocaleDateString('default', { day: 'numeric' })
-let month = date.toLocaleDateString('default', { month: 'long' });
+let longDay = currentDate.toLocaleDateString('default', { weekday: 'long' });
+let shortDay = currentDate.toLocaleDateString('default', { day: 'numeric' })
+let month = currentDate.toLocaleDateString('default', { month: 'long' });
 
 //set days of week at bottom to next 5 days
-let day2 = new Date(date.getTime() + 86400000).toLocaleDateString('default', { weekday: 'long' });
-let day3 = new Date(date.getTime() + 86400000 * 2).toLocaleDateString('default', { weekday: 'long' });
-let day4 = new Date(date.getTime() + 86400000 * 3).toLocaleDateString('default', { weekday: 'long' });
-let day5 = new Date(date.getTime() + 86400000 * 4).toLocaleDateString('default', { weekday: 'long' });
-let day6 = new Date(date.getTime() + 86400000 * 5).toLocaleDateString('default', { weekday: 'long' });
+let day2 = new Date(currentDate.getTime() + 86400000).toLocaleDateString('default', { weekday: 'long' });
+let day3 = new Date(currentDate.getTime() + 86400000 * 2).toLocaleDateString('default', { weekday: 'long' });
+let day4 = new Date(currentDate.getTime() + 86400000 * 3).toLocaleDateString('default', { weekday: 'long' });
+let day5 = new Date(currentDate.getTime() + 86400000 * 4).toLocaleDateString('default', { weekday: 'long' });
+let day6 = new Date(currentDate.getTime() + 86400000 * 5).toLocaleDateString('default', { weekday: 'long' });
 
 //Set Date in Navbar to Current Date
 dateNav.textContent = `${longDay}, ${month} ${shortDay}`;
@@ -52,83 +59,136 @@ day4Day.textContent = `${day4}`
 day5Day.textContent = `${day5}`
 day6Day.textContent = `${day6}`
 
-//get lat and lon
+//gen lat and lon
 let lat;
 let lon;
 
 //Set up location and what to do if it succeeds
 navigator.geolocation.getCurrentPosition(success, errorFunc);
-{
-    coords: {
-        latitude: 10;
-        longitude: 20;
-    }
-}
 
 //Success of Grabbing Location
-function success(position) {
+async function success(position) {
 
-    //set lat and lon to coords
-    lat = position.coords.latitude
-    lon = position.coords.longitude
-
-    //fetch the API for weather
-    async function apiCall() {
-        const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
-        const weatherData = await weatherPromise.json();
-        console.log(weatherData.list[0].dt)
-        
-        //Use API Data to make and populate info
-        let currentTemp = Math.round(weatherData.list[0].main.temp);
-        centerTemp.textContent = `${currentTemp}°`
-        
-        let currentMin = Math.round(weatherData.list[0].main.temp_min);
-        let currentMax = Math.round(weatherData.list[0].main.temp_max);
-        minMax.textContent = `Min ${currentMin}°, Max ${currentMax}°`
-
-        //fetch the API for Reverse Geolocation
-        const locationPromise = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${apiKey}`)
-        const locationData = await locationPromise.json();
-
-        //Add an if statement to check for if in US
-
-        //set location in center to location
-        let placeName = locationData[0].name;
-        let stateName = locationData[0].state;
-
-        //add an if no state do country
-
-        centerLocation.textContent = `${placeName}, ${stateName}`;
-
-        //Set up an iterator to parse through the dt
-        //convert the dt to dates
-        //Grab the Hi/Low of those dates by mathing the highest of the his and lowest of the lows and then save those
-        //populate into ids
-        
-        //We need arrays of each list object that associates to each day, so day 1 would be the list objects of current day
-        //make a 2d array of days with their 
-
-     let tempMaxs = [];
-     let tempMins = [];
-
-
-        for(let i = 0; i < weatherData.list.length; i++){
-            let unixTime = new Date(weatherData.list[i].dt * 1000)
-            //console.log(unixTime.toLocaleDateString('en-US'))
-            //console.log(unixTime)
-            //console.log(weatherData.list[i].main.temp_max);
-            //console.log(weatherData.list[i].main.temp_min);
-            tempMaxs.push(weatherData.list[i].main.temp_max)
-            tempMins.push(weatherData.list[i].main.temp_min)
-        }
-        //im thinking about using .map to do this better lol
-        console.log(tempMaxs)
-        console.log(tempMins)
+    if (searchBar.value) {
+        const citySearch = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchBar.value}&limit=5&appid=${apiKey}`);
+        const cityName = await citySearch.json();
+        lat = cityName[0].lat;
+        lon = cityName[0].lon;
     }
-    apiCall();
+    else {
+        //set lat and lon to coords
+        lat = position.coords.latitude
+        lon = position.coords.longitude
+    }
+
+
+    //Current Weather API
+    const weatherPromise = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
+    const weatherData = await weatherPromise.json()
+
+    //Forecast Weather API
+    const forecastPromise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
+    const forecastData = await forecastPromise.json();
+
+    //fetch the API for Reverse Geolocation
+    const locationPromise = await fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${apiKey}`)
+    const locationData = await locationPromise.json();
+
+    //Gen Max and Min Arrays
+    let tempMaxs1 = [];
+    let tempMins1 = [];
+    let tempMaxs2 = [];
+    let tempMins2 = [];
+    let tempMaxs3 = [];
+    let tempMins3 = [];
+    let tempMaxs4 = [];
+    let tempMins4 = [];
+    let tempMaxs5 = [];
+    let tempMins5 = [];
+    let tempMaxs6 = [];
+    let tempMins6 = [];
+
+    tempMaxs1.push(weatherData.main.temp_max)
+    tempMins1.push(weatherData.main.temp_min)
+
+    for (let i = 0; i < forecastData.list.length; i++) {
+        let unixTime = new Date(forecastData.list[i].dt * 1000)
+        let unixTime2 = new Date(forecastData.list[i].dt * 1000 + 86400000)
+        let unixTime3 = new Date(forecastData.list[i].dt * 1000 + (86400000 * 2))
+        let unixTime4 = new Date(forecastData.list[i].dt * 1000 + (86400000 * 3))
+        let unixTime5 = new Date(forecastData.list[i].dt * 1000 + (86400000 * 4))
+        let unixTime6 = new Date(forecastData.list[i].dt * 1000 + (86400000 * 5))
+
+        if (unixTime.toLocaleDateString('en-US') === date.toLocaleDateString('en-US')) {
+            tempMaxs1.push(forecastData.list[i].main.temp_max)
+            tempMins1.push(forecastData.list[i].main.temp_min)
+        }
+        else if (unixTime2.toLocaleDateString('en-US') === date.toLocaleDateString('en-US')) {
+            tempMaxs2.push(forecastData.list[i].main.temp_max)
+            tempMins2.push(forecastData.list[i].main.temp_min)
+        }
+        else if (unixTime3.toLocaleDateString('en-US') === date.toLocaleDateString('en-US')) {
+            tempMaxs3.push(forecastData.list[i].main.temp_max)
+            tempMins3.push(forecastData.list[i].main.temp_min)
+        }
+        else if (unixTime4.toLocaleDateString('en-US') === date.toLocaleDateString('en-US')) {
+            tempMaxs4.push(forecastData.list[i].main.temp_max)
+            tempMins4.push(forecastData.list[i].main.temp_min)
+        }
+        else if (unixTime5.toLocaleDateString('en-US') === date.toLocaleDateString('en-US')) {
+            tempMaxs5.push(forecastData.list[i].main.temp_max)
+            tempMins5.push(forecastData.list[i].main.temp_min)
+        }
+        else if (unixTime6.toLocaleDateString('en-US') === date.toLocaleDateString('en-US')) {
+            tempMaxs6.push(forecastData.list[i].main.temp_max)
+            tempMins6.push(forecastData.list[i].main.temp_min)
+        }
+    }
+
+    //Use API Data to make and populate info
+    let currentTemp = Math.round(weatherData.main.temp);
+    centerTemp.textContent = `${currentTemp}°`
+
+    let currentMin = Math.round(Math.min(...tempMins1));
+    let currentMax = Math.round(Math.max(...tempMaxs1));
+    minMax.textContent = `Min ${currentMin}°, Max ${currentMax}°`
+
+    day2High.textContent = `${Math.round(Math.max(...tempMaxs2))}°`
+    day2Low.textContent = `${Math.round(Math.min(...tempMins2))}°`
+
+    day3High.textContent = `${Math.round(Math.max(...tempMaxs3))}°`
+    day3Low.textContent = `${Math.round(Math.min(...tempMins3))}°`
+
+    day4High.textContent = `${Math.round(Math.max(...tempMaxs4))}°`
+    day4Low.textContent = `${Math.round(Math.min(...tempMins4))}°`
+
+    day5High.textContent = `${Math.round(Math.max(...tempMaxs5))}°`
+    day5Low.textContent = `${Math.round(Math.min(...tempMins5))}°`
+
+    day6High.textContent = `${Math.round(Math.max(...tempMaxs6))}°`
+    day6Low.textContent = `${Math.round(Math.min(...tempMins6))}°`
+
+    //Add an if statement to check for if in US
+
+    //set location in center to location
+    let placeName = locationData[0].name;
+    let stateName = locationData[0].state;
+    centerLocation.textContent = `${placeName}, ${stateName}`;
+
+    //add an if no state do country
+
 }
 //failure of grabbing location
-function errorFunc() { };
+function errorFunc(error) {
+    error.message
+}
 
+searchBar.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        console.log(searchBar.value)
+        success(searchBar.value)
+        e.preventDefault();
+        return false;
 
-
+    }
+});
